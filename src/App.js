45dbd62b21a5f4bebf845376bbeee57821df5fe4
vishal <you@example.com>
2020-08-15 14:07:39 +0530

    minor changees

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-// import './App.css';
 import './index.css';
-// import './assets/dist/css/lib/bootstrap.min.css';
-// import './assets/dist/css/swipe.min.css';
-// import './assets/dist/img/favicon.png';
 
 // import SignIn from './Component/Auth/signIn';
 import Chat from './Component/Chat/Chat';
 
 function App() {
   return (
-    // <div className="App">
-    //  <SignIn />
     <Chat />
-    // </div>
   );
 }
 
